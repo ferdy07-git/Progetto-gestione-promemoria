1d713857d02c3df4f5919fc3a561192b9a1ceead
ferdy07-git <ferdypoke07@gmail.com>
2025-04-11 09:06:14 +0200

    Inizializzazione dell'index e struttura generale del programma

---
+++ source/index.js
@@ -0,0 +1,39 @@
+"use strict"
+
+import * as f from './utils.js'
+
+import promptSync from 'prompt-sync'
+
+const input=promptSync();
+
+let promemoria=[];
+let nome,ora,stato;
+
+let scelta;
+do{
+    console.log("Menù: ");
+    console.log("1)Crea promemoria\n2)Visualizza promemoria\n3)Modifica promemoria\n4)Rimuovi promemoria\n0)Esci");
+    scelta=input(":");
+
+    switch(scelta){
+        case 1:{
+
+            break;
+        }
+        case 2:{
+
+            break;
+        }
+        case 3:{
+
+            break;
+        }
+        case 4:{
+
+            break;
+        }
+    }
+
+}while(scelta!=0);
+
+
