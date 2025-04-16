cf58c85fa6a996497fb2adb6c5f0c9a4cf2d2c31
ferdy07-git <ferdypoke07@gmail.com>
2025-04-16 15:06:39 +0200

    Richiamata la funzione RicercaPromemoria

--- source/index.js
+++ source/index.js
@@ -14,10 +14,10 @@ do{
     console.log("Menù: ");
     console.log("1)Crea promemoria\n2)Visualizza promemoria\n3)Ricerca promemoria\n4)Modifica promemoria\n5)Rimuovi promemoria\n6)Completa attività\n0)Esci");
     scelta=input(":");
+    console.log("\n");
 
     switch(scelta){
         case '1':{
-            console.log("\n");
             nome=input("Dai un nome al promemoria: ");
             ora=input("A che ora devo ricordartelo? ")
             stato=false;
@@ -29,13 +29,15 @@ do{
             break;
         }
         case '2':{
-            console.log("\n");
             f.VisualizzaPromemoria(promemoria);
             console.log("\n");
             break;
         }
         case '3':{
-
+            nome=input("Inserisci il nome del promemoria: ");
+            console.log("\n");
+            f.RicercaPromemoria(promemoria,nome);
+            console.log("\n");
             break;
         }
         case '4':{
