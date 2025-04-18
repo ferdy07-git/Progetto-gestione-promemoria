0dc3c64376668c4a5642630a63b6d8a678a70d9d
ferdy07-git <ferdypoke07@gmail.com>
2025-04-18 10:41:15 +0200

    Documentato con JSDoc

--- source/utils.js
+++ source/utils.js
@@ -1,5 +1,11 @@
 "use strict"
 
+/**
+ * Crea un promemoria
+ * @param {string} n - nome promemoria
+ * @param {Number} o - orario promemoria
+ * @param {boolean} s - stato di completamento promemoria
+ */
 export function CreaPromemoria(n,o,s){
     const nuovoPromemoria = {
         nome:n,
@@ -9,10 +15,22 @@ export function CreaPromemoria(n,o,s){
     return nuovoPromemoria;
 }
 
+/**
+ *  Controlla se all'interno dell'array è presente un duplicato
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ * @param {string} nome - nome preso da input 
+ * 
+ * @returns {boolean} true se c'è un duplicato, false se non ci sono duplicati
+ */
 export function ControllaDuplicato(promemoria, nome) {
     return promemoria.some(p => p.nome === nome);
 }
 
+/**
+ * Visualizza l'intero array di oggetti con stampa formattata
+ * 
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ */
 export function VisualizzaPromemoria(promemoria){
     promemoria.forEach(x => {
         console.log(`Nome promemoria: ${x.nome}`);
@@ -26,6 +44,13 @@ export function VisualizzaPromemoria(promemoria){
     });
 }
 
+/**
+ * Filtra i promemoria in base a criteri come nome,ora e stato di completamento
+ * 
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ * @param {string} filtra - criterio di filtraggio
+ * @returns {Array<Object>} Ritorna l'array ordinato in base al criterio selezionato
+ */
 export function FiltraPromemoria(promemoria, filtra) {
     switch (filtra){
         case '1':{
@@ -66,6 +91,11 @@ export function FiltraPromemoria(promemoria, filtra) {
     return promemoria;
 }
 
+/**
+ * Ricerca del promemoria tramite il nome preso da input
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ * @param {string} n - nome del promemoria da ricercare
+ */
 export function RicercaPromemoria(promemoria,n){
     promemoria.forEach(x => {
         if(x.nome===n){
@@ -81,6 +111,14 @@ export function RicercaPromemoria(promemoria,n){
     });
 }
 
+
+/**
+ * Modifica il promemoria
+ * @param {string} n - vecchio nome promemoria
+ * @param {*} promemoria - Array contenente i promemoria
+ * @param {*} n_mod - nuovo nome promemoria
+ * @param {*} o - orario promemoria
+ */
 export function ModificaPromemoria(n,promemoria,n_mod,o){
     promemoria.forEach(x => {
         if(x.nome===n){
@@ -90,6 +128,11 @@ export function ModificaPromemoria(n,promemoria,n_mod,o){
     });
 }
 
+/**
+ * Rimuove il promemoria tramite il nome preso da input
+ * @param {string} n - nome del promemoria da rimuovere
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ */
 export function RimuoviPromemoria(n, promemoria) {
     for (let i = promemoria.length - 1; i >= 0; i--) {
         if (promemoria[i].nome === n) {
@@ -98,6 +141,11 @@ export function RimuoviPromemoria(n, promemoria) {
     }
 }
 
+/**
+ * Completa l'attività 
+ * @param {string} n - nome dell'attività da completare 
+ * @param {Array<Object>} promemoria - Array contenente i promemoria
+ */
 export function CompletaAttivita(n,promemoria){
     promemoria.forEach(x => {
         if(x.nome===n){
